c6fa2f08233f91eb2ea777a28687420e056005d5
asidikfauzi <asidikfauzi@gmail.com>
2025-02-14 19:37:58 +0700

    feat: add constant error

--- internal/pkg/constant/error_constant.go
+++ internal/pkg/constant/error_constant.go
@@ -8,4 +8,8 @@ const (
 	CategoryNotFound            string = "category not found"
 	CategoryUnprocessableEntity string = "category unprocessable entity"
 	CategoryAlreadyExists       string = "category already exists"
+
+	MeasurementNotFound            string = "measurement not found"
+	MeasurementUnprocessableEntity string = "measurement unprocessable entity"
+	MeasurementAlreadyExists       string = "measurement already exists"
 )
