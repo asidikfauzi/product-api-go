6d479503d9999b30e99d2b980db1c12258750776
asidikfauzi <asidikfauzi@gmail.com>
2025-02-17 01:31:23 +0700

    mod: modify error constant

--- internal/pkg/constant/error_constant.go
+++ internal/pkg/constant/error_constant.go
@@ -1,28 +1,26 @@
 package constant
 
-const (
-	InvalidJsonPayload     string = "invalid request payload"
-	InvalidQueryParameters string = "invalid query parameters"
-	FailedToLoadTimeZone   string = "failed to load timezone: %w"
-	UnexpectedError        string = "unexpected error: %v"
-	JsonUnmarshalError     string = "json unmarshal error: %v"
-	JsonMarshalError       string = "json marshal error: %v"
+import "errors"
 
-	KeyRedisNotExists string = "key not exists"
-	RedisGetError     string = "redis get error: %v"
-	RedisSetError     string = "redis set error: %v"
-	RedisDeleteError  string = "redis delete error: %v"
-
-	CategoryNotFound            string = "category not found"
-	SomeCategoryNotFound        string = "some categories not found"
-	CategoryUnprocessableEntity string = "category unprocessable entity"
-	CategoryAlreadyExists       string = "category already exists"
-
-	MeasurementNotFound            string = "measurement not found"
-	MeasurementUnprocessableEntity string = "measurement unprocessable entity"
-	MeasurementAlreadyExists       string = "measurement already exists"
-
-	ProductNotFound            string = "product not found"
-	ProductUnprocessableEntity string = "product unprocessable entity"
-	ProductAlreadyExists       string = "product already exists"
+var (
+	CategoryNotFound               = errors.New("category not found")
+	InvalidJsonPayload             = errors.New("invalid request payload")
+	InvalidQueryParameters         = errors.New("invalid query parameters")
+	FailedToLoadTimeZone           = errors.New("failed to load timezone")
+	UnexpectedError                = errors.New("unexpected error")
+	JsonUnmarshalError             = errors.New("json unmarshal error")
+	JsonMarshalError               = errors.New("json marshal error")
+	KeyRedisNotExists              = errors.New("key not exists")
+	RedisGetError                  = errors.New("redis get error")
+	RedisSetError                  = errors.New("redis set error")
+	RedisDeleteError               = errors.New("redis delete error")
+	SomeCategoryNotFound           = errors.New("some categories not found")
+	CategoryUnprocessableEntity    = errors.New("category unprocessable entity")
+	CategoryAlreadyExists          = errors.New("category already exists")
+	MeasurementNotFound            = errors.New("measurement not found")
+	MeasurementUnprocessableEntity = errors.New("measurement unprocessable entity")
+	MeasurementAlreadyExists       = errors.New("measurement already exists")
+	ProductNotFound                = errors.New("product not found")
+	ProductUnprocessableEntity     = errors.New("product unprocessable entity")
+	ProductAlreadyExists           = errors.New("product already exists")
 )
